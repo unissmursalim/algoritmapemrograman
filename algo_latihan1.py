b3bbec07beba6af825656ec55ef131104961b0c8
unissmursalim <136144614+unissmursalim@users.noreply.github.com>
2023-06-10 12:56:15 +0700

    Update algo_latihan1.py
    Menambahkan source code tentang penilaian KHS Mahasiswa

--- algo_latihan1.py
+++ algo_latihan1.py
@@ -1,1 +1,35 @@
-maaf belum diisi, nantikan acara selanjutnya
+# Setting Variabel
+nim  = "20123022"
+nama = "Hercules"
+kelas= "2A"
+prodi= "Informatika"
+
+#Setting Variabel nilai
+bhs_indo =76
+bhs_ing  =70
+pd       =90
+mtk      =65
+kal1     =67
+so       =75
+
+# Perhitungan
+rata = (bhs_indo+bhs_ing+pd+mtk+kal1+so)/6
+
+# Cetak Hasil
+print("----------------------------------------")
+print("NIM Anda             :",nim)
+print("Nama Lengkap Anda    :",nama)
+print("Kelas                :",kelas)
+print("Program Studi        :",prodi)
+print("----------------------------------------")
+print("         Hasil Kartu Hasil Studi")
+print("----------------------------------------")
+print("1. Bahasa Indonesia          ",bhs_indo)
+print("2. Bahasa Inggris            ",bhs_ing)
+print("3. Pemrograman Dasar         ",pd)
+print("4. Matematika                ",mtk)
+print("5. Kalkulus 1                ",kal1)
+print("6. Sistem Operasi            ",so)
+print("----------------------------------------")
+print("Rata -rata                   ",rata)
+print("----------------------------------------")
